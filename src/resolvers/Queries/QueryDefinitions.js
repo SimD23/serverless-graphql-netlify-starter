4f23151cb2623103c3c30fa46043a528a2a5b6ce
Sim <simd23@gmail.com>
2020-03-19 17:36:12 +0000

    recover to init

--- src/resolvers/Queries/QueryDefinitions.js
+++
@@ -1,8 +0,0 @@
-const { gql } = require("apollo-server");
-
-module.export = `
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-  }
-`;
--- src/resolvers/resolvers.js
+++
@@ -1,8 +0,0 @@
-const Queries = require("./Queries/Queries");
-const Mutations = require("./Mutations/Mutations");
-
-module.exports = {
-  // A map of functions which return data for the schema
-  Query: Queries
-  //   Mutation: Mutations
-};
